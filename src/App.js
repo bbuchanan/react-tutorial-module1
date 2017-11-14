567f48e6ff378690dd04e75592bad7c532f6d174
Bill Buchanan <bbuchanan@gmail.com>
2017-11-14 15:00:06 -0500

    changing persons state array on button click

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ class App extends Component {
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 38 }
-    ]
+    ],
+    otherState: 'some other value'
   }
 
   switchNameHandler = () => {
-    console.log("Was clicked!")
+    //console.log("Was clicked!")
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
 
   render() {
