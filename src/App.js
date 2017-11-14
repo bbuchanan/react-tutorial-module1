9e1bcf0f2cfced5ae2df5f7cf5e4f2b87701ae7f
Bill Buchanan <bbuchanan@gmail.com>
2017-11-14 14:53:48 -0500

    implemented basic state and click handler.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 38 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was clicked!")
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Hi, I'm a React app</h1>
       <p>This is really working!</p>
-      <Person name="Max" age="39"/>
-      <Person name="Annie" age="33">My Hobbies: Racing</Person>
-      <Person name="Bitchassslutasswhore" age="23" />
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
 
