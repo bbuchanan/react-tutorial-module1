9ffe6a4c6fc006820c934938331d82138bace36a
Bill Buchanan <bbuchanan@gmail.com>
2017-11-19 14:55:17 -0500

    toggle conditional with button lecture

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 38 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -33,6 +34,13 @@ class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -47,19 +55,21 @@ class App extends Component {
       <h1>Hi, I'm a React app</h1>
       <p>This is really working!</p>
       <button style={style}
-       onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-      <Person
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age}/>
-      <Person
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this, 'Max!')}
-        changed={this.nameChangedHandler}
-        >My Hobbies: Racing</Person>
-      <Person
-        name={this.state.persons[2].name}
-        age={this.state.persons[2].age}/>
+       onClick={this.togglePersonsHandler}>Toggle Persons</button>
+       {this.state.showPersons ? <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}
+          >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div> : null}
       </div>
     );
 
