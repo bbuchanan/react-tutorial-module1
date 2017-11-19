6e51c5c6429b2f083e0a272727d9513864a7a64c
Bill Buchanan <bbuchanan@gmail.com>
2017-11-19 15:17:23 -0500

    modifying state by deleting persons using the spread operator

--- src/App.js
+++ src/App.js
@@ -13,15 +13,10 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    //console.log("Was clicked!")
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
   }
 
   nameChangedHandler = (event) => {
@@ -55,8 +50,8 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={this.state.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={this.state.age} />
           })}
       </div>
       )
