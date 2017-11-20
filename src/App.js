c23af8a0a8c5d44cd9ad8b4f3d6472c1b5844f4f
Bill Buchanan <bbuchanan@gmail.com>
2017-11-20 16:34:26 -0500

    basic dynamic styles

--- src/App.js
+++ src/App.js
@@ -45,7 +45,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -60,18 +61,27 @@ class App extends Component {
           {this.state.persons.map((person, index) => {
             return <Person click={() => this.deletePersonHandler(index)}
             name={person.name}
-            age={this.state.age}
+            age={person.age}
             key={person.id}
             changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
       </div>
       )
+      style.backgroundColor = 'red'
+    }
+
+    let classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
     }
 
     return (
       <div className="App">
       <h1>Hi, I'm a React app</h1>
-      <p>This is really working!</p>
+      <p className={classes.join(' ')}>This is really working!</p>
       <button style={style}
        onClick={this.togglePersonsHandler}>Toggle Persons</button>
        {persons}
